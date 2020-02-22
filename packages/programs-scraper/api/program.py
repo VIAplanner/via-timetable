a7c18839e237f36176ac4df2a35cebeba212f4b3
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-02-22 00:49:45 -0500

    almost completed program scraper

--- packages/programs-scraper/api/program.py
+++ packages/programs-scraper/api/program.py
@@ -12,17 +12,15 @@ class Program:
     level: str
     code: str
     notes: List[str]
-    degree: str
-    credits: float
     courses: Dict[int, Union[List[str]]]
+    program_type: str
 
     def __init__(self):
         self.name = ""
         self.level = ""
         self.code = ""
         self.notes = []
-        self.degree = ""
-        self.credits = 0.0
+        self.program_type = -1
         self.courses = {1: [], 2: [], 3: [], 4: []}
 
     def set_name(self, name: str):
@@ -34,27 +32,26 @@ class Program:
     def set_code(self, code: str):
         self.code = code
 
-    def add_notes(self, note: str):
+    def add_note(self, note: str):
         self.notes.append(note)
     
     def add_course(self, course: str):
         if course not in self.courses[int(course[3])]:
             self.courses[int(course[3])].append(course)
-
-    def set_degree(self, degree: str):
-        self.degree = degree
-
-    def set_credits(self, credits: float):
-        self.credits = credits
+    
+    def set_program_type(self, program_type: str):
+        self.program_type = program_type
 
     def to_json(self):
+        for i in range(1, 5):
+            self.courses[i].sort()
+        
         curr_json = {
             "name": self.name,
             "level": self.level,
             "code": self.code,
+            "type": self.program_type,
             "notes": self.notes,
-            "degree": self.degree,
-            "credits": self.credits,
             "courses": self.courses
         }
         return curr_json
--- packages/programs-scraper/api/program_scraper.py
+++ packages/programs-scraper/api/program_scraper.py
@@ -4,8 +4,9 @@ import json
 from bs4 import BeautifulSoup, element
 from subject import Subject
 from program import Program
-from typing import Tuple
+from typing import Tuple, Dict, List
 from tqdm import tqdm  # progress bar magic
+import pprint
 
 
 # test if a subject is no longer offered
@@ -16,20 +17,9 @@ def subject_exist(url: str) -> bool:
         'dl', class_='title2')  # if the there are any program advisors
     return exist is not None
 
-
-def program_exist(title_soup) -> bool:
-    test_soup = title_soup.next_sibling
-    if repr(test_soup) == repr('\n'):
-        test_soup = test_soup.next_sibling
-
-    if isinstance(test_soup, element.NavigableString):
-        if "discontinued" in test_soup.string:
-            return False
-    elif isinstance(test_soup, element.Tag):
-        if "discontinued" in test_soup.text:
-            return False
-
-    return True
+# test if a subject is no longer offered
+def program_exist(cnl: List[str], all_program_types: Dict[str, str]) -> bool:
+    return all_program_types.get(cnl[0]) is not None
 
 
 # finds all the subject ids
@@ -47,8 +37,58 @@ def all_subject_ids(url: str) -> Tuple[str]:
     return tuple(ids)
 
 
-# subject_ids = all_subject_ids("https://student.utm.utoronto.ca/calendar//program_list.pl")
-subject_ids = ('9',)
+# test if a program title is reached
+def title_reached(test_soup) -> bool:
+    return isinstance(test_soup, element.Tag) and test_soup.name == 'p' and test_soup['class'][0] == "title_program"
+
+
+# return a dictionary with the name of the program as the key and its type as the value 
+def find_all_types() -> Dict:
+    url = "https://www.utm.utoronto.ca/registrar/office-registrar-publications/program-selection-guide"
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+    program_list_soup = soup.find('table', cellpadding="0").tbody.find_all('tr')[1:]
+    type_dict = {}
+
+    for type_soup in program_list_soup:
+        type_soup = type_soup.find_all('td')[3:5]
+        type_dict[type_soup[0].text.strip('\n')] = type_soup[1].text.strip('\n')
+    
+    return type_dict
+
+
+# finds the program name, code and its level 
+def code_name_level_finder(title: str) -> List[str]:
+
+    normal_pattern = "[A-Z]{5}[0-9]{4}"
+    weird_pattern = "[A-Z]{9}[0-9A-Z]"
+    curr_pattern = normal_pattern
+    all_values = []
+
+    if len(re.findall(curr_pattern, title)) == 0:
+        curr_pattern = weird_pattern
+
+
+    all_values.append(re.findall(curr_pattern, title)[0])
+    all_values.append(re.split(curr_pattern, title)[1])
+    level_section = re.split(curr_pattern, title)[0]
+
+    if "Specialist" in level_section:
+        all_values.append("Specialist")
+    elif "Major" in level_section:
+        all_values.append("Major")
+    elif "Minor" in level_section:
+        all_values.append("Minor")
+    
+    return all_values
+        
+
+
+
+subject_ids = all_subject_ids("https://student.utm.utoronto.ca/calendar//program_list.pl")
+# subject_ids = ('9',)
+
+all_program_types = find_all_types()
 
 for subject_id in tqdm(subject_ids):
 
@@ -58,57 +98,74 @@ for subject_id in tqdm(subject_ids):
     all_degrees = ["HBA", "HBSc", "BBA", "BCom"]
 
     if subject_exist(url):
-        subject = Subject()
+        curr_subject = Subject()
         title = soup.find('p', class_='titlestyle')
-        body = soup.find('div', class_='centralpos').find(
-            'div', class_='contentpos')
+        body = soup.find('div', class_='centralpos').find('div', class_='contentpos')
         notes = body.find('ol', class_="numbers")
         programs = body.find_all('p', class_="title_program")
+        
         if notes is not None:
             notes = notes.find_all('li')
 
-        subject.set_name(title.text.split(' (')[0])
+        curr_subject.set_name(title.text.split(' (')[0])
 
         for degree in all_degrees:
             if degree in title.text:
-                subject.add_degree(degree)
+                curr_subject.add_degree(degree)
 
         if notes is not None:
             for note in notes:
-                subject.add_note(note.text.replace("\r", "").strip())
+                curr_subject.add_note(note.text.replace("\r", "").strip())
 
         for program_soup in programs:
             curr_program = Program()
-            if not program_exist(program_soup):
+            cnl = code_name_level_finder(program_soup.text)
+
+            if not program_exist(cnl, all_program_types):
                 continue
-            
-            curr_program.set_name(program_soup.text.split(' ')[2][9:] + ' ' + ' '.join(program_soup.text.split(' ')[3:]))
-            curr_program.set_code(''.join(program_soup.text.split(' ')[2][0:8]))
-            curr_program.set_level(program_soup.text.split(' ')[0])
 
-            limited_enrollment_courses = program_soup.find_next_sibling(
-                'div', class_='lim_enrol')
+            cnl = code_name_level_finder(program_soup.text)
 
-            if limited_enrollment_courses is not None:
-                limited_enrollment_courses = limited_enrollment_courses.find_all(
-                    'a')
-                if limited_enrollment_courses is not None:
-                    for course_soup in limited_enrollment_courses:
-                        if len(course_soup.text) == 8:
-                            curr_program.add_course(course_soup.text)
+            curr_program.set_code(cnl[0])
+            curr_program.set_name(cnl[1])
+            curr_program.set_level(cnl[2])
 
-            table_courses = program_soup.find_next_sibling(
-                'table', class_='tab_adm')
+            if all_program_types.get(curr_program.code) is not None:
+                curr_program.set_program_type(all_program_types[curr_program.code])
+            else:
+                print(curr_subject.name + "\n")
+                print(curr_program.name + "\n")
 
-            if table_courses is not None:
-                table_courses = table_courses.find_all('a')
-                if table_courses is not None:
-                    for course_soup in table_courses:
-                        if len(course_soup.text) == 8:
-                            curr_program.add_course(course_soup.text)
+            next_line_soup = program_soup.next_sibling
 
-            subject.add_program(curr_program)
+            while(not title_reached(next_line_soup)):
+
+                if isinstance(next_line_soup, element.Tag):
+
+                    # notes of the program
+                    if next_line_soup.name == 'div' and next_line_soup['class'][0] == "lim_enrol":
+                        all_notes = next_line_soup.find_all('li')
+
+                        if all_notes is not None:
+                            for note_soup in all_notes:
+                                curr_program.add_note(note_soup.text.replace("\r", "").replace("\t", "").strip())
+                    
+                    # courses of the program
+                    all_courses = next_line_soup.find_all('a')
+                    if all_courses is not None:
+                        for course_soup in all_courses:
+                            if len(course_soup.text) == 8:
+                                curr_program.add_course(course_soup.text)
 
 
-        with open('../output/subjects/' + subject.name + '.json', 'w') as file:
-            json.dump(subject.to_json(), file)
+                if next_line_soup is None:
+                    break
+                else: 
+                    next_line_soup = next_line_soup.next_sibling
+
+
+            curr_subject.add_program(curr_program)
+
+
+        with open('../output/subjects/' + curr_subject.name + '.json', 'w') as file:
+            json.dump(curr_subject.to_json(), file)
