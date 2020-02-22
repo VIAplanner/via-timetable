993110f63cb804677c6b12ba1e33707f26b958f1
Brian <brianli.li@mail.utoronto.ca>
2020-02-22 16:48:28 -0500

    Co-authored-by: williamchau123 <billy.chau@mail.utoronto.ca>

--- packages/timetable-planner/planner.js
+++ packages/timetable-planner/planner.js
@@ -1,45 +1,45 @@
-// Given a set of courses, is there a valid timetable?
-// What is a valid timetable?
-// A valid timetable has no conflicts
+/*Given a set of courses, is there a valid timetable?
+What is a valid timetable?
+A valid timetable has no conflicts
 
-// Next step is to determine if there is a conflict, given a set of courses
+Next step is to determine if there is a conflict, given a set of courses
 
-// Notes
-// We'll be using info from the meeting sections field of the course JSON
-// The start and end times are in seconds from 12:00am 
-
-
-//To check conflict, compare the start time of each course to see if it is overlapped by other courses:
-// A function that determine whether overlapping exists
-// input: A set of course objects with its corresponding start and end time
-// course_list = {"CSC108H5F2019LEC0101":[{"Monday":[ "61400", "72800"], "Wednesday":["50400", "54000"],"Friday":["50400", "54000"]}]}
-// timetable = {"Monday":[[]], "Tuesday":[[]],"Wednesday":[[]],"Thursday":[[]],"Friday":[[]]}
-// add all the course time to each correspond date, and then compare within the list
-// if any conflict appears, return invalid
-// else return valid
-// for each time interval in the list, take the start time, loop through the list to check if the time is inside the interval of other element
-//  
+Notes
+We'll be using info from the meeting sections field of the course JSON
+The start and end times are in seconds from 12:00am 
 
 
+To check conflict, compare the start time of each course to see if it is overlapped by other courses:
+A function that determine whether overlapping exists
+input: A set of course objects with its corresponding start and end time
+course_list = {"CSC108H5F2019LEC0101":[{"Monday":[ "61400", "72800"], "Wednesday":["50400", "54000"],"Friday":["50400", "54000"]}]}
+timetable = {"Monday":[[]], "Tuesday":[[]],"Wednesday":[[]],"Thursday":[[]],"Friday":[[]]}
+add all the course time to each correspond date, and then compare within the list
+if any conflict appears, return invalid
+else return valid
+for each time interval in the list, take the start time, loop through the list to check if the time is inside the interval of other element
+ 
 
 
-// If I'm looking at a course json
-// Determine the types of unique meeting sections
-// Create a configuration
 
+
+If I'm looking at a course json
+Determine the types of unique meeting sections
+Create a configuration
+*/
 function overlap(timetable) {
     //parameter: dictionary of date as the key, time intervals as the value 
     //timetable = {"MONDAY":[[]], "TUESDAY":[[]],"WEDNESDAY":[[]],"THURSDAY":[[]],"FRIDAY":[[]]}
-    for (var date in timetable) {
-        for (var time in timetable[date]) {
-            if (timetable[date].length > 1) {
-                for (var time_2 in timetable[date]) {
+    for (var day in timetable) {
+        for (var time in timetable[day]) {
+            if (timetable[day].length > 1) {
+                for (var time_2 in timetable[day]) {
                     time_2 = +time + +1;
-                    if(time_2 < timetable[date].length){
-                        var time_1_0 = timetable[date][time][0];
-                        var time_1_1 = timetable[date][time][1];
-                        var time_2_0 = timetable[date][time_2][0];
-                        var time_2_1 = timetable[date][time_2][1];
+                    if (time_2 < timetable[day].length) {
+                        var time_1_0 = timetable[day][time][0];
+                        var time_1_1 = timetable[day][time][1];
+                        var time_2_0 = timetable[day][time_2][0];
+                        var time_2_1 = timetable[day][time_2][1];
                         if ((time_1_0 >= time_2_0 && time_1_0 <= time_2_1) || (time_1_1 >= time_2_0 && time_1_1 <= time_2_1)) {
                             return false;
                         }
@@ -52,33 +52,25 @@ function overlap(timetable) {
 
 }
 
-function bucket_course_by_day(course_list) {
-    //return: timetable = {"MONDAY":[], "TUESDAY":[],"WEDNESDAY":[],"THURSDAY":[],"FRIDAY":[]}
-    //parameter: dictionary of courses as the key with their time as the value 
-    //dic_course: {"title":[{"date":[time]}]}
-    //date: ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"]
-    //time: [start, end] start, end: int in seconds STARTING FROM 12:00AM
-    //title: String (should not affect the function for now)
+function bucket_course_by_day(course_list, invalid_time) {
+    /*
+    return: timetable = {"MONDAY":[], "TUESDAY":[],"WEDNESDAY":[],"THURSDAY":[],"FRIDAY":[]}
+    parameter: dictionary of courses as the key with their time as the value 
+    course_list: {"title":{"day":[time]}}
+    invalid_time: {"invalid_time":{"day1":[[time]]}}
+    day: ["MONDAY","TUESDAY","WEDNESDAY","THURSDAY","FRIDAY"]
+    time: [start, end] start, end: int in seconds STARTING FROM 12:00AM
+    title: String (should not affect the function for now)
+    */
     var timetable = { "MONDAY": [], "TUESDAY": [], "WEDNESDAY": [], "THURSDAY": [], "FRIDAY": [] }
     for (var title in course_list) {
-        for (var dates in course_list[title]) {
-            for (var date in course_list[title][dates]) {
-                if (date == "MONDAY") {
-                    timetable["MONDAY"].push(course_list[title][dates][date]);
-                }
-                if (date == "TUESDAY") {
-                    timetable["TUESDAY"].push(course_list[title][dates][date]);
-                }
-                if (date == "WEDNESDAY") {
-                    timetable["WEDNESDAY"].push(course_list[title][dates][date]);
-                }
-                if (date == "THURSDAY") {
-                    timetable["THURSDAY"].push(course_list[title][dates][date]);
-                }
-                if (date == "FRIDAY") {
-                    timetable["FRIDAY"].push(course_list[title][dates][date]);
-                }
-            }
+        for (var day in course_list[title]) {
+            timetable[day].push(course_list[title][day]);
+        }
+    }
+    for (var day in invalid_time["invalid_time"]) {
+        for (var time_section in invalid_time["invalid_time"][day]) {
+            timetable[day].push(invalid_time["invalid_time"][day][time_section])
         }
     }
     if (overlap(timetable)) {
@@ -86,7 +78,6 @@ function bucket_course_by_day(course_list) {
     } else {
         return "inValid Timetable";
     }
-
 }
 
-module.exports = transform;
+module.exports = bucket_course_by_day;
--- packages/timetable-planner/planner.test.js
+++ packages/timetable-planner/planner.test.js
@@ -1,6 +1,8 @@
-const transform = require("./planner");
+const bucket_course_by_day = require("./planner");
 
 test("append correctly?", ()=>{
-    var dic_courses = {"CSC108H5F2019LEC0101":[{"MONDAY":[ "61400", "72800"], "WEDNESDAY":["50400", "54000"],"FRIDAY":["25000", "35000"]}],"CSC148H5F2019LEC0101":[{FRIDAY:["20000", "30000"]}]};
-    expect(transform(dic_courses)).toBe("inValid Timetable");
-})
+    var courses_list = {"CSC108H5F2019LEC0101":{"MONDAY":[ "61400", "72800"], "WEDNESDAY":["50400", "54000"],"FRIDAY":["25000", "35000"]},"CSC148H5F2019LEC0101":{FRIDAY:["20000", "30000"]}};
+    var invalid_time = {"invalid_time":{"MONDAY":[["61400", "72800"]]}}
+    expect(bucket_course_by_day(courses_list)).toBe("inValid Timetable");
+});
+npm run test
