3242a2301c0174154da9198de31bede4e6c49446
William Chau <billy.chau@mail.utoronto.ca>
2020-02-22 16:58:04 -0800

    Fix jest

--- packages/timetable-planner/planner.test.js
+++ packages/timetable-planner/planner.test.js
@@ -3,6 +3,5 @@ const bucket_course_by_day = require("./planner");
 test("append correctly?", ()=>{
     var courses_list = {"CSC108H5F2019LEC0101":{"MONDAY":[ "61400", "72800"], "WEDNESDAY":["50400", "54000"],"FRIDAY":["25000", "35000"]},"CSC148H5F2019LEC0101":{FRIDAY:["20000", "30000"]}};
     var invalid_time = {"invalid_time":{"MONDAY":[["61400", "72800"]]}}
-    expect(bucket_course_by_day(courses_list)).toBe("inValid Timetable");
+    expect(bucket_course_by_day(courses_list, invalid_time)).toBe("inValid Timetable");
 });
-npm run test
