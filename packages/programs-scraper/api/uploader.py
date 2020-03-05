5fb1477b75e60ecbf93dd2b42dc06ef4ff0d4176
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-03-05 14:04:25 -0500

    finished uploading subjects to mongodb

--- packages/programs-scraper/api/uploader.py
+++ packages/programs-scraper/api/uploader.py
@@ -2,19 +2,18 @@ import pymongo
 import json
 from pymongo import MongoClient
 import os
+from tqdm import tqdm  # progress bar magic
+
 
 # the mongodb server
 cluster = MongoClient(
     "mongodb+srv://user_1:coursetools@coursetoolscluster-wjb51.mongodb.net/test?retryWrites=true&w=majority")
 db = cluster["UofT"]
-collection = db["Courses"]
-courses_directory = "courses/"
+collection = db["Subjects"]
+courses_directory = "../output/subjects/"
 
 # adding all JSON from local to server
-count = 0
-for course in os.listdir(courses_directory):
+for course in tqdm(os.listdir(courses_directory)):
     with open(courses_directory + course) as f:
         file_data = json.load(f)
-    count += 1
     collection.insert_one(file_data)
-    print(count)
