53c81ca1287d771f5bafe0f4b145e7edb0e4eba2
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-03-27 18:33:14 -0400

    added styles to optimization setting

--- packages/app/src/components/OptimizationSettings.vue
+++ packages/app/src/components/OptimizationSettings.vue
@@ -1,22 +1,25 @@
 <template>
-  <v-container fluid>
-    <v-row>
-      <v-col>
-        <v-checkbox
-          v-for="weekday in weekdays"
-          :key="weekday"
-          v-model="selected"
-          :label="weekday"
-          :value="weekday"
-        ></v-checkbox>
-      </v-col>
-    </v-row>
+  <v-container fluid class="settings-container">
+	<p class="header-style">
+		Optimzation Settings
+	</p>
     <v-row>
       <v-col>
         <v-radio-group v-model="selectedIdleOption" :mandatory="false">
-          <v-radio v-for="message in idleOptions" :key="message" :label="message" :value="message"></v-radio>
+          <v-radio v-for="message in idleOptions" :key="message" :label="message" :value="message" color="#3F5D79"></v-radio>
         </v-radio-group>
       </v-col>
+    </v-row>
+    <v-row justify="center">
+      <v-checkbox
+        class="checkbox-style"
+        v-for="weekday in weekdays"
+        :key="weekday"
+        v-model="selected"
+        :label="weekday"
+        :value="weekday"
+		color="#3F5D79"
+      ></v-checkbox>
     </v-row>
   </v-container>
 </template>
@@ -29,11 +32,28 @@ export default {
       selectedDays: [],
       weekdays: ["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"],
       idleOptions: ["Maximize Idle Time", "Minimize Idle Time"],
-      selectedIdleOption: ''
+      selectedIdleOption: ""
     };
   }
 };
 </script>
 
 <style scoped>
+
+	.settings-container {
+		margin: 0px;
+		width: 550px;
+		background: #ededed;
+		border-radius: 25px;
+	}
+
+	.checkbox-style {
+		margin-left: 10px;
+	}
+
+	.header-style {
+		font-size: 25px;
+		color: #3F5D79;
+	}
+
 </style>
