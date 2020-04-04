5d87979335561831b13c195342109e10073ed1dd
William <billy.chau@mail.utoronto.ca>
2020-04-04 02:27:51 -0700

    Updated test cases and fixed relative bugs

--- packages/timetable-planner/src/selects/selects.spec.ts
+++ packages/timetable-planner/src/selects/selects.spec.ts
@@ -64,22 +64,71 @@ const emptyTimetable: Timetable = {
 }
 
 const resultEmptyTimetable: Timetable = {
-    MONDAY:[],
+    MONDAY:[ {
+        code: 'CSC108H5FL0101',
+
+         day: 'MONDAY',
+         duration: 3600,
+         end: 36000,
+         instructors: [
+          'A Petersen',
+         ],
+         location: 'MN 1270',
+         start: 32400,
+       },],
     THURSDAY:[],
     TUESDAY:[],
-    WEDNESDAY:[],
-    FRIDAY:[]
+    WEDNESDAY:[ {
+        code: 'CSC108H5FL0101',
+
+         day: 'WEDNESDAY',
+         duration: 3600,
+         end: 36000,
+         instructors: [
+          'A Petersen',
+         ],
+         location: 'MN 1270',
+         start: 32400,
+       },],
+    FRIDAY:[ {
+        code: 'CSC108H5FL0101',
+
+         day: 'FRIDAY',
+         duration: 3600,
+         end: 36000,
+         instructors: [
+          'A Petersen',
+         ],
+         location: 'MN 1270',
+         start: 32400,
+       },]
 }
 
 const switchPracticeTimetable: Timetable = {
-    MONDAY:[],
+    MONDAY:[{
+               code: 'CSC108H5FP0101',
+               day: 'MONDAY',
+               duration: 7200,
+               end: 39600,
+               instructors: [],
+               location: 'DH 2010',
+               start: 32400,
+             },],
     THURSDAY:[],
     TUESDAY:[],
     WEDNESDAY:[],
     FRIDAY:[]
 }
 const resultSwitchPracticeTimetable: Timetable = {
-    MONDAY:[],
+    MONDAY:[{
+               code: 'CSC108H5FP0102',
+               day: 'MONDAY',
+               duration: 7200,
+               end: 46800,
+               instructors: [],
+               location: 'DH 2010',
+               start: 39600,
+             },],
     THURSDAY:[],
     TUESDAY:[],
     WEDNESDAY:[],
@@ -87,13 +136,175 @@ const resultSwitchPracticeTimetable: Timetable = {
 }
 
 const lockSection: string[] = [
-
+    "CSC108H5FP0102"
 ]
 const lockTimetables: Timetable[] = [
-
-] 
+    {
+        FRIDAY: [
+          {
+           code: 'CSC108H5FL0101',
+  
+            day: 'FRIDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+        MONDAY: [
+          {
+           code: 'CSC108H5FL0101',
+            day: 'MONDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+          {
+           code: 'CSC108H5FP0102',
+            day: 'MONDAY',
+            duration: 7200,
+            end: 46800,
+            instructors: [],
+            location: 'DH 2010',
+            start: 39600,
+          },
+        ],
+        THURSDAY: [],
+        TUESDAY: [],
+        WEDNESDAY: [
+          {
+           code: 'CSC108H5FL0101',
+            day: 'WEDNESDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+      },
+      {
+        FRIDAY: [
+          {
+           code: 'CSC108H5FL0101',
+  
+            day: 'FRIDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+        MONDAY: [
+        ],
+        THURSDAY: [{
+            code: 'CSC108H5FL0101',
+             day: 'THURSDAY',
+             duration: 3600,
+             end: 36000,
+             instructors: [
+              'A Petersen',
+             ],
+             location: 'MN 1270',
+             start: 32400,
+           },
+           {
+            code: 'CSC108H5FP0101',
+             day: 'THURSDAY',
+             duration: 7200,
+             end: 46800,
+             instructors: [],
+             location: 'DH 2010',
+             start: 39600,
+           },],
+        TUESDAY: [],
+        WEDNESDAY: [
+          {
+           code: 'CSC108H5FL0101',
+            day: 'WEDNESDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+      },
+{
+    MONDAY:[],
+    THURSDAY:[],
+    TUESDAY:[],
+    WEDNESDAY:[],
+    FRIDAY:[],
+}]
 const resultLockTimetables: Timetable[] = [
-
+    {
+        FRIDAY: [
+          {
+           code: 'CSC108H5FL0101',
+  
+            day: 'FRIDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+        MONDAY: [
+          {
+           code: 'CSC108H5FL0101',
+            day: 'MONDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+          {
+           code: 'CSC108H5FP0102',
+            day: 'MONDAY',
+            duration: 7200,
+            end: 46800,
+            instructors: [],
+            location: 'DH 2010',
+            start: 39600,
+          },
+        ],
+        THURSDAY: [],
+        TUESDAY: [],
+        WEDNESDAY: [
+          {
+           code: 'CSC108H5FL0101',
+            day: 'WEDNESDAY',
+            duration: 3600,
+            end: 36000,
+            instructors: [
+             'A Petersen',
+            ],
+            location: 'MN 1270',
+            start: 32400,
+          },
+        ],
+      },
 ]
 
 test('Test switch section', async t => {
--- packages/timetable-planner/src/selects/selects.ts
+++ packages/timetable-planner/src/selects/selects.ts
@@ -8,17 +8,20 @@
  * @returns {Timetable}
  */
 const switchSection = (timetable: Timetable, sectionName: string, course: Course): Timetable =>{
+    
     let tempSections: TimetableSection[] = []
+    // Search for the section in the course and convert to Timetable section format
     for (const section of course.meeting_sections){
-        if (section.code === sectionName.substring(10)){
+        if (section.code === sectionName.substring(9)){
             for (const time of section.times){
                 tempSections.push({code: course.code + section.code, instructors: section.instructors, ...time})
             }
         }
     }
     const days = ['MONDAY', 'TUESDAY', 'WEDNESDAY', 'THURSDAY', 'FRIDAY']
+    // filter out the duplicate section type from the timetable
     for (const day of days){
-        timetable[day] = timetable[day].filter(section => section.code.substring(0,) != sectionName)
+        timetable[day] = timetable[day].filter(section => section.code.substring(0,10) != sectionName.substring(0,10))
     }
     for (const section of tempSections){
         timetable[section.day].push(section)
@@ -39,7 +42,7 @@ const lockSections = (sections: string[], timetables: Timetable[]): Timetable[] 
     for (const timetable of timetables){
         for (const day of days){
             for (const section of timetable[day]){
-                if (sections.includes(section.code) || !tempTimetables.includes(timetable)){
+                if (sections.includes(section.code) && !tempTimetables.includes(timetable)){
                     tempTimetables.push(timetable)
                 }
             }
