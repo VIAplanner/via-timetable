be869dde80c0c8c5646de4d5891b9f161f7b1ce9
ArsalaBangash <arsalabangash@gmail.com>
2020-04-05 18:56:27 -0400

    Removed timetable-planner module

--- packages/app/src/timetable-planner/module/combinations/combinations.d.ts
+++
@@ -1,3 +0,0 @@
-declare const courseMeetingSectionCombinations: (course: Course) => CourseMeetingSectionCombinations;
-declare const courseCombinations: (courseMeetingSectionCombos: CourseMeetingSectionCombinations[]) => MeetingSection[][];
-export { courseMeetingSectionCombinations, courseCombinations };
