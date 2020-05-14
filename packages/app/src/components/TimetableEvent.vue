0265482fdcdf3096152246144743b42bda6aeefd
Yingxiaoyang Hu <shawn.hu@mail.utoronto.ca>
2020-05-14 15:34:30 -0400

    working on the new optimization

--- packages/app/src/components/TimetableEvent.vue
+++ packages/app/src/components/TimetableEvent.vue
@@ -1,162 +1,224 @@
 <template>
-  <div>
-    <div v-if="event.start > 0">
-      <v-dialog v-model="dialog" scrollable width="825px" @input="atInput">
-        <template v-slot:activator="{ on }">
-          <div
-            @mouseover="hovered = true"
-            @mouseleave="hovered = false"
-            v-on="on"
-            class="event"
-            :class="durationClass(event.start, event.end)"
-            :style=" { background: getCourseColor(event.code) }"
-          >
-            <h4 class="course-code">{{event.code}}</h4>
+    <div>
+        <div v-if="event.start > 0">
+            <v-dialog
+                v-model="dialog"
+                scrollable
+                width="825px"
+                @input="atInput"
+            >
+                <template v-slot:activator="{ on }">
+                    <div
+                        @mouseover="hovered = true"
+                        @mouseleave="hovered = false"
+                        v-on="on"
+                        class="event"
+                        :class="durationClass(event.start, event.end)"
+                        :style="{ background: getCourseColor(event.code) }"
+                    >
+                        <h4 class="course-code">{{ event.code }}</h4>
 
-            <div class="lock-button">
-              <v-btn dark @click.stop="lockToggle" v-if="locked" icon>
-                <v-icon>mdi-lock</v-icon>
-              </v-btn>
-              <v-btn dark @click.stop="lockToggle" v-if="!locked && hovered" icon>
-                <v-icon>mdi-lock-open</v-icon>
-              </v-btn>
+                        <div class="lock-button">
+                            <v-btn
+                                dark
+                                @click.stop="lockToggle"
+                                v-if="locked"
+                                icon
+                            >
+                                <v-icon>mdi-lock</v-icon>
+                            </v-btn>
+                            <v-btn
+                                dark
+                                @click.stop="lockToggle"
+                                v-if="!locked && hovered"
+                                icon
+                            >
+                                <v-icon>mdi-lock-open</v-icon>
+                            </v-btn>
+                        </div>
+
+                        <div style="margin-left: 3px;">
+                            {{ event.sectionCode }}
+                        </div>
+
+                        <div style="position: relative;">
+                            <div class="align-left">
+                                {{ getFormattedTime(event.start, event.end) }}
+                            </div>
+                            <div class="align-right">{{ event.location }}</div>
+                        </div>
+                    </div>
+                </template>
+                <course-section-picker
+                    v-on:done="dialog = false"
+                    :code="event.code"
+                    ref="popUp"
+                />,
+            </v-dialog>
+        </div>
+        <div
+            v-else
+            class="event empty-event one-hour"
+            :style="dynamicColor"
+            @mouseover="
+                dynamicColor = { background: '#e6e6e6' };
+                hovered = true;
+            "
+            @mouseleave="
+                dynamicColor = { background: 'white' };
+                hovered = false;
+            "
+        >
+            <div v-if="hovered">
+                <v-row>
+                    <v-col>
+                        <p class="center">Lock This Time</p>
+                        <div style=" text-align:center;">
+                            <v-btn
+                                v-if="locked"
+                                @click.stop="reverseLockStatus"
+                                icon
+                            >
+                                <v-icon>mdi-lock</v-icon>
+                            </v-btn>
+                            <v-btn
+                                v-else
+                                @click.stop="reverseLockStatus"
+                                icon
+                            >
+                                <v-icon>mdi-lock-open</v-icon>
+                            </v-btn>
+                        </div>
+                    </v-col>
+                </v-row>
             </div>
-
-            <div style="margin-left: 3px;">{{event.sectionCode}}</div>
-
-            <div style="position: relative;">
-              <div class="align-left">{{getFormattedTime(event.start, event.end)}}</div>
-              <div class="align-right">{{event.location}}</div>
-            </div>
-          </div>
-        </template>
-        <course-section-picker v-on:done="dialog=false" :code="event.code" ref="popUp"/>,
-      </v-dialog>
+        </div>
     </div>
-    <div class="event empty-event one-hour" v-else></div>
-  </div>
 </template>
 
 <script>
 import CourseSectionPicker from "../components/CourseSectionPicker";
-import { mapGetters } from 'vuex'
+import { mapGetters } from "vuex";
 
-const convertSecondsToHours = seconds => {
-  return seconds / 3600;
+const convertSecondsToHours = (seconds) => {
+    return seconds / 3600;
 };
 
 export default {
-  name: "timetable-event",
-  props: {
-    event: {
-      type: Object,
-      default: () => {}
-    }
-  },
-  components: {
-    CourseSectionPicker
-  },
-  destroyed() {
-    console.log('destroyed')
-    this.locked = false;
-  },
-  data() {
-    return {
-      locked: false,
-      hovered: false,
-      dialog: false
-    };
-  },
-  computed: {
-    ...mapGetters(["getCourseColor"])
-  },
-  methods: {
-    atInput() {
-      var courseSectionPicker = this.$refs.popUp;
-      if (typeof courseSectionPicker != 'undefined') {
-        courseSectionPicker.resetSelectedMeetingSections()
-      }
+    name: "timetable-event",
+    props: {
+        event: {
+            type: Object,
+            default: () => {},
+        },
     },
-    reverseLockStatus() {
-      this.locked = !this.locked;
+    components: {
+        CourseSectionPicker,
     },
-    lockToggle() {
-      this.$emit('toggleLock', {event: this.event, status: this.locked})
+    data() {
+        return {
+            locked: false,
+            hovered: false,
+            dialog: false,
+            dynamicColor: { background: "white" },
+        };
     },
-    durationClass(start, end) {
-      const duration = convertSecondsToHours(end - start);
-      if (duration === 1) {
-        return "one-hour";
-      } else if (duration === 2) {
-        return "two-hours";
-      } else if (duration === 3) {
-        return "three-hours";
-      }
+    computed: {
+        ...mapGetters(["getCourseColor"]),
     },
-    getFormattedTime(start, end) {
-      var s = (start / 3600) % 12;
-      var e = (end / 3600) % 12;
-      if (s == 0) {
-        s = 12;
-      }
-      if (e == 0) {
-        e = 12;
-      }
-      return `${s}:00 - ${e}:00`;
+    methods: {
+        atInput() {
+            var courseSectionPicker = this.$refs.popUp;
+            if (typeof courseSectionPicker != "undefined") {
+                courseSectionPicker.resetSelectedMeetingSections();
+            }
+        },
+        reverseLockStatus() {
+            this.locked = !this.locked;
+        },
+        lockToggle() {
+            this.$emit("toggleLock", this.event);
+        },
+        durationClass(start, end) {
+            const duration = convertSecondsToHours(end - start);
+            if (duration === 1) {
+                return "one-hour";
+            } else if (duration === 2) {
+                return "two-hours";
+            } else if (duration === 3) {
+                return "three-hours";
+            }
+        },
+        getFormattedTime(start, end) {
+            var s = (start / 3600) % 12;
+            var e = (end / 3600) % 12;
+            if (s == 0) {
+                s = 12;
+            }
+            if (e == 0) {
+                e = 12;
+            }
+            return `${s}:00 - ${e}:00`;
+        },
     },
-  }
 };
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css?family=Montserrat&display=swap");
 * {
-  font-family: "Montserrat", sans-serif;
+    font-family: "Montserrat", sans-serif;
+}
+
+.center {
+    color: black;
+    text-align: center;
+    margin: 0 !important;
 }
 
 .event {
-  border: 0.2px solid gray;
-  color: white;
-  padding: 8px;
-  position: relative;
-  cursor: pointer;
+    border: 0.2px solid gray;
+    color: white;
+    padding: 8px;
+    position: relative;
+    cursor: pointer;
 }
 .empty-event {
-  background: white !important;
-  border: 0.2px solid gray;
-  cursor: default;
+    background: white;
+    border: 0.2px solid gray;
+    cursor: default;
 }
 .one-hour {
-  height: 84px;
+    height: 84px;
 }
 
 .two-hours {
-  height: 168px;
+    height: 168px;
 }
 
 .three-hours {
-  height: 252px;
+    height: 252px;
 }
 
 .course-code {
-  margin-bottom: 3px;
-  margin-left: 3px;
+    margin-bottom: 3px;
+    margin-left: 3px;
 }
 
 .align-left {
-  position: absolute;
-  left: 3px;
+    position: absolute;
+    left: 3px;
 }
 
 .align-right {
-  position: absolute;
-  right: 5px;
+    position: absolute;
+    right: 5px;
 }
 
 .lock-button {
-  position: absolute;
-  right: 3px;
-  top: 3px;
-  z-index: 1;
+    position: absolute;
+    right: 3px;
+    top: 3px;
+    z-index: 1;
 }
 </style>
