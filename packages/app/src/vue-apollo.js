24307c0cfff25209923b246a8380c792cd07b542
Shawn Hu <shawnhu97@gmail.com>
2020-06-04 18:10:32 -0400

    fixing apollo issue

--- packages/app/src/vue-apollo.js
+++ packages/app/src/vue-apollo.js
@@ -9,7 +9,7 @@ Vue.use(VueApollo)
 const AUTH_TOKEN = 'apollo-token'
 
 // Http endpoint
-const httpEndpoint = 'http://www.api.uoftcoursetools.tech'
+const httpEndpoint = 'https://www.api.uoftcoursetools.tech'
 // Files URL root
 export const filesRoot = process.env.VUE_APP_FILES_ROOT || httpEndpoint.substr(0, httpEndpoint.indexOf('/graphql'))
 
