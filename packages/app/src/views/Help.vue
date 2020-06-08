df0696f5469f19701799f4e905e256b22b37c9eb
Shawn Hu <shawnhu97@gmail.com>
2020-06-08 14:30:36 -0400

    adding menu

--- packages/app/src/views/Help.vue
+++ packages/app/src/views/Help.vue
@@ -11,7 +11,7 @@
                 <v-spacer></v-spacer>
                 <v-btn text href="https://uoftcoursetools.tech/" target="blank">
                     <v-icon left>mdi-file-document</v-icon>
-                    Docs
+                    API Docs
                 </v-btn>
                 <v-btn
                     text
@@ -73,18 +73,18 @@
                     <v-col cols="5" class="pb-0">
                         <v-row align="center" justify="end" style="height: 450px;">
                             <div style="text-align: right">
-                                <h3>Flexibility</h3>
-                                <h1 style="margin-bottom: 40px">
-                                    Block Times Off
+                                <h3>Generate</h3>
+                                <h1 style="margin-bottom: 20px">
+                                    Schedule Automatically
                                 </h1>
                                 <h3 class="font-weight-medium">
-                                    Can't go to school on Mondays?
+                                    Simply choose the course on the search bar
                                 </h3>
                                 <h3
                                     style="margin-bottom: 40px;"
                                     class="font-weight-medium"
                                 >
-                                    Just block that day off and we will reschedule.
+                                    and we will make a timetable for you
                                 </h3>
                             </div>
                         </v-row>
@@ -99,13 +99,76 @@
                         </div>
                     </v-col>
                 </v-row>
+                <v-row justify="center" class="pt-12">
+                    <v-col class="pb-0">
+                        <div style="text-align:center">
+                            <v-img
+                                class="center"
+                                src="../assets/img3.gif"
+                                max-width="800"
+                            ></v-img>
+                        </div>
+                    </v-col>
+                    <v-col cols="5" class="pb-0">
+                        <v-row align="center" justify="start" style="height: 450px;">
+                            <div>
+                                <h3>Flexibility</h3>
+                                <h1 style="margin-bottom: 20px">
+                                    Block Times Off
+                                </h1>
+                                <h3 class="font-weight-medium">
+                                    Can't go to school on Mondays?
+                                </h3>
+                                <h3
+                                    style="margin-bottom: 40px;"
+                                    class="font-weight-medium"
+                                >
+                                    Just block that day off and we will reschedule
+                                </h3>
+                            </div>
+                        </v-row>
+                    </v-col>
+                </v-row>
+                <v-row
+                    justify="center"
+                    style="height: 650px; background-color: #FCFCFC"
+                >
+                    <v-col style="text-align: center" cols="8">
+                        <h1 class="ma-3">How to Use</h1>
+                        <v-carousel cycle height="500" show-arrows-on-hover>
+                            <v-carousel-item v-for="(path, text) in slideData" :key=text>
+                                <v-sheet color="white">
+                                    <v-row>
+                                        <v-col>
+                                            <p style="color: black">
+                                                {{text}}
+                                            </p>
+                                            <v-img
+                                                class="center"
+                                                :src=""
+                                            />
+                                        </v-col>
+                                    </v-row>
+                                </v-sheet>
+                            </v-carousel-item>
+                        </v-carousel>
+                    </v-col>
+                </v-row>
             </v-container>
         </v-content>
     </v-app>
 </template>
 
 <script>
-export default {};
+export default {
+    data(){
+        return {
+            slideData: {
+                "Search for a course": "slide1.gif"
+            }
+        }
+    }
+};
 </script>
 
 <style scoped>
