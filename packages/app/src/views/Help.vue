1fcb6ffaa8d46ef450f8aa2655a7f473725d7ad3
Shawn Hu <shawnhu97@gmail.com>
2020-06-08 13:47:54 -0400

    added section 2

--- packages/app/src/views/Help.vue
+++ packages/app/src/views/Help.vue
@@ -22,9 +22,9 @@
                     GitHub
                 </v-btn>
             </v-app-bar>
-            <v-container fluid >
+            <v-container fluid>
                 <v-row>
-                    <v-col cols="7">
+                    <v-col cols="7" class="pb-0">
                         <div style="text-align:center">
                             <v-img
                                 class="center"
@@ -33,14 +33,17 @@
                             ></v-img>
                         </div>
                     </v-col>
-                    <v-col cols="4">
+                    <v-col cols="4" class="pb-0">
                         <v-row align="center" style="height: 450px;">
                             <div>
                                 <h1>Making Your Timetable,</h1>
                                 <h1 style="margin-bottom: 40px">
                                     Should Never Be a Chore
                                 </h1>
-                                <h3 style="margin-bottom: 40px" class="font-weight-medium">
+                                <h3
+                                    style="margin-bottom: 40px"
+                                    class="font-weight-medium"
+                                >
                                     Have you always dreaded making your timetable?
                                     Searching through Acorn desperately trying to fit
                                     together your courses? Well, we built a solution
@@ -60,6 +63,40 @@
                                 </v-btn>
                             </div>
                         </v-row>
+                    </v-col>
+                </v-row>
+                <v-row
+                    style="background-color: #FCFCFC"
+                    justify="center"
+                    class="pt-12"
+                >
+                    <v-col cols="5" class="pb-0">
+                        <v-row align="center" justify="end" style="height: 450px;">
+                            <div style="text-align: right">
+                                <h3>Flexibility</h3>
+                                <h1 style="margin-bottom: 40px">
+                                    Block Times Off
+                                </h1>
+                                <h3 class="font-weight-medium">
+                                    Can't go to school on Mondays?
+                                </h3>
+                                <h3
+                                    style="margin-bottom: 40px;"
+                                    class="font-weight-medium"
+                                >
+                                    Just block that day off and we will reschedule.
+                                </h3>
+                            </div>
+                        </v-row>
+                    </v-col>
+                    <v-col class="pb-0">
+                        <div style="text-align:center">
+                            <v-img
+                                class="center"
+                                src="../assets/img2.gif"
+                                max-width="800"
+                            ></v-img>
+                        </div>
                     </v-col>
                 </v-row>
             </v-container>
