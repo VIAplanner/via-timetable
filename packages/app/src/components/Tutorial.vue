8afc5f9a6e0bddb314365a0e65aac1b58ffeab2f
Shawn Hu <shawnhu97@gmail.com>
2020-06-12 15:30:26 -0400

    added localStoarge to detect first time visit

--- packages/app/src/components/Tutorial.vue
+++ packages/app/src/components/Tutorial.vue
@@ -11,13 +11,13 @@
                 </v-btn>
             </v-card-title>
 
-            <v-carousel
-                hide-delimiter-background
-                show-arrows-on-hover
-                height="480"
-            >
+            <v-carousel hide-delimiter-background show-arrows-on-hover height="480">
                 <v-carousel-item v-for="tut in allTut" :key="tut.step">
-                    <v-sheet style="border-radius: 0px" :color="tut.backgroundColor" height="100%">
+                    <v-sheet
+                        style="border-radius: 0px"
+                        :color="tut.backgroundColor"
+                        height="100%"
+                    >
                         <v-row class="ma-0" style="width: 600px">
                             <v-col>
                                 <p class="text-h5 ml-3 mb-0">{{ tut.step }}</p>
@@ -38,9 +38,14 @@
 <script>
 import genColor from "color-generator";
 export default {
+    mounted() {
+        if (!localStorage.visited) {
+            localStorage.visited = true;
+        }
+    },
     data() {
         return {
-            dialog: true,
+            dialog: !localStorage.visited,
             allTut: [
                 {
                     step: "Step 1",
@@ -48,7 +53,7 @@ export default {
                     path: require("../assets/tut1.gif"),
                     description:
                         "Just click on a course and we will generate a timetable",
-                    backgroundColor: genColor(0.7, 0.85).hexString()
+                    backgroundColor: genColor(0.7, 0.85).hexString(),
                 },
                 {
                     step: "Step 2",
@@ -56,15 +61,14 @@ export default {
                     path: require("../assets/tut2.gif"),
                     description:
                         "Click on the pencil icon or the course itself to adjust the times",
-                    backgroundColor: genColor(0.7, 0.85).hexString()
+                    backgroundColor: genColor(0.7, 0.85).hexString(),
                 },
                 {
                     step: "Step 3",
                     title: "Select and lock",
                     path: require("../assets/tut3.gif"),
-                    description:
-                        "Lock times in place and we won't change it",
-                    backgroundColor: genColor(0.7, 0.85).hexString()
+                    description: "Lock times in place and we won't change it",
+                    backgroundColor: genColor(0.7, 0.85).hexString(),
                 },
                 {
                     step: "Step 4",
@@ -72,7 +76,7 @@ export default {
                     path: require("../assets/tut4.gif"),
                     description:
                         "Click on the top right to switch between fall and winter",
-                    backgroundColor: genColor(0.7, 0.85).hexString()
+                    backgroundColor: genColor(0.7, 0.85).hexString(),
                 },
             ],
         };
