b9d18210b1652abbe184b7a439b90574be75824d
Shawn Hu <shawnhu97@gmail.com>
2020-06-17 15:07:43 -0400

    added google analytics

--- packages/app/src/main.js
+++ packages/app/src/main.js
@@ -1,27 +1,32 @@
 /* eslint-disable */
 
-import '@babel/polyfill'
-import 'mutationobserver-shim'
-import Vue from 'vue'
-import App from './App.vue'
-import router from './router'
-import store from './store'
-import vuetify from './plugins/vuetify';
-import 'roboto-fontface/css/roboto/roboto-fontface.css'
-import '@mdi/font/css/materialdesignicons.css'
-import { createProvider } from './vue-apollo'
-import ToggleButton from 'vue-js-toggle-button'
+import "@babel/polyfill";
+import "mutationobserver-shim";
+import Vue from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import vuetify from "./plugins/vuetify";
+import "roboto-fontface/css/roboto/roboto-fontface.css";
+import "@mdi/font/css/materialdesignicons.css";
+import { createProvider } from "./vue-apollo";
+import ToggleButton from "vue-js-toggle-button";
 import VueMobileDetection from "vue-mobile-detection";
+import VueAnalytics from "vue-analytics";
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-Vue.use(ToggleButton)
+Vue.use(ToggleButton);
 Vue.use(VueMobileDetection);
+Vue.use(VueAnalytics, {
+    id: "UA-165919387-2",
+    router
+});
 
 new Vue({
-  router,
-  store,
-  vuetify,
-  apolloProvider: createProvider(),
-  render: h => h(App)
-}).$mount('#app')
+    router,
+    store,
+    vuetify,
+    apolloProvider: createProvider(),
+    render: (h) => h(App),
+}).$mount("#app");
