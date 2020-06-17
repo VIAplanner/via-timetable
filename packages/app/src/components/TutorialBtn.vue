fa6b51869afacbe386c99eafa03dc80a7a516f41
Shawn Hu <shawnhu97@gmail.com>
2020-06-17 14:57:19 -0400

    added tooltips to speeddial

---
+++ packages/app/src/components/TutorialBtn.vue
@@ -0,0 +1,46 @@
+<template>
+    <v-row @mouseover="hovered = true" @mouseleave="hovered = false">
+        <v-tooltip left v-if="hovered">
+            <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                    fab
+                    dark
+                    small
+                    color="orange"
+                    @click="setTutorialPopup(true)"
+                    v-bind="attrs"
+                    v-on="on"
+                >
+                    <v-icon>mdi-teach</v-icon>
+                </v-btn>
+            </template>
+            <span>Tutorial</span>
+        </v-tooltip>
+        <v-btn v-else fab dark small color="orange" @click="setTutorialPopup(true)">
+            <v-icon>mdi-teach</v-icon>
+        </v-btn>
+    </v-row>
+</template>
+
+<script>
+import { mapMutations } from "vuex";
+export default {
+    data() {
+        return {
+            hovered: false,
+        };
+    },
+    methods: {
+        ...mapMutations(["setTutorialPopup"]),
+    },
+};
+</script>
+
+<style scoped>
+.tooltip {
+    position: absolute;
+    z-index: 1;
+    top: 22%;
+    right: 105%;
+}
+</style>
