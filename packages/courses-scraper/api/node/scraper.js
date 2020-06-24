0b58f78309218ace92ae50d0e39393d47e78e356
Shawn Hu <shawnhu97@gmail.com>
2020-06-24 12:59:10 -0400

    scraper is partially working

--- packages/courses-scraper/api/node/scraper.js
+++ packages/courses-scraper/api/node/scraper.js
@@ -1,16 +1,39 @@
 const puppeteer = require('puppeteer');
-async function main() {
+
+const main = async () => {
+
     const browser = await puppeteer.launch({
-        // headless: false,
+        headless: false,
     });
     const page = await browser.newPage();
-    await page.goto('https://student.utm.utoronto.ca/timetable/?course=CSC108H5', { waitUntil: 'networkidle0' });
-    let bodyHTML = await page.evaluate(() => document.body.innerHTML);
-    console.log(bodyHTML)
-    // await page.screenshot({
-    //     path: 'example.png'
-    // });
-    await browser.close();
+    await page.goto('https://student.utm.utoronto.ca/timetable/', { waitUntil: 'networkidle0' });
+
+    // get list of all courses offered
+    let courseCodes = await page.evaluate(() => {
+        let allCoursesDiv = document.querySelectorAll("div[class='selectize-dropdown-content']")[3].querySelectorAll("div")
+        let codes = []
+        for (let courseDiv of allCoursesDiv) {
+            codes.push(courseDiv.innerText)
+        }
+        return codes
+    });
+
+
+    await page.goto(`https://student.utm.utoronto.ca/timetable?course=${courseCodes[0]}`, { waitUntil: 'networkidle0' });
+    
+    let allInfo = await page.evaluate(() => {
+        let allInfoDivs = document.querySelectorAll("tr.meeting_section")[0].querySelectorAll("td")
+        let data = []
+        for (let infoDiv of allInfoDivs) {
+            data.push(infoDiv.innerText)
+        }
+        return data
+    });
+
+    console.log(allInfo)
+
+    // console.log(courseCodes)
+    // await browser.close();
 }
 
 // Start the script
