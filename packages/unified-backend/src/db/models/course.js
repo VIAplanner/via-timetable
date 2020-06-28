6398e594d9095a69814571b86459de9db0c5d5a5
Shawn Hu <shawnhu97@gmail.com>
2020-06-28 18:50:55 -0400

    completed course model

---
+++ packages/unified-backend/src/db/models/course.js
@@ -0,0 +1,120 @@
+const mongoose = require("mongoose")
+const Schema = mongoose.Schema
+
+const timeSchema = new Schema({
+    day: {
+        type: String,
+        required: true
+    },
+    start: {
+        type: Number,
+        required: true
+    },
+    end: {
+        type: Number,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    location: {
+        type: String,
+        default: "NA"
+    }
+})
+
+const meetingSectionSchema = new Schema({
+    code: {
+        type: String,
+        required: true
+    },
+    instructors: {
+        type: [String],
+        required: true
+    },
+    times: {
+        type: [timeSchema],
+        validate(value){
+            if (value.length === 0){
+                throw new Error("Times must be provided")
+            }
+        } 
+    },
+    size: {
+        type: Number,
+        default: 0
+    },
+    enrolment: {
+        type: Number,
+        default: 0
+    },
+    notes: {
+        type: String,
+        default: "NA"
+    }
+})
+
+
+const courseSchema = new Schema({
+    id: {
+        type: String,
+        required: true,
+    },
+    code: {
+        type: String,
+        required: true
+    },
+    name: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    division: {
+        type: String,
+        default: "NA"
+    },
+    department: {
+        type: String,
+        default: "NA"
+    },
+    prerequisites: {
+        type: String,
+        default: "NA"
+    },
+    exclusions: {
+        type: String,
+        default: "NA"
+    },
+    level: {
+        type: Number,
+        default: 0
+    },
+    campus: {
+        type: String,
+        default: "NA"
+    },
+    term: {
+        type: String,
+        default: "NA"
+    },
+    breadths: {
+        type: Array,
+        default: []
+    },
+    meeting_sections: {
+        type: [meetingSectionSchema],
+        validate(value){
+            if (value.length === 0){
+                throw new Error("Meeting sections must be provided")
+            }
+        } 
+    }
+})
+
+const Course = mongoose.model("Course", courseSchema)
+
+module.exports = Course
