4f67b68a39b6d38ba860a99882776ac194814c9f
Shawn Hu <shanwhu97@gmail.com>
2020-06-30 12:18:01 -0400

    updated ssl redirect

--- packages/unified-backend/src/index.js
+++ packages/unified-backend/src/index.js
@@ -1,11 +1,13 @@
 require("./db/mongoose")
 const express = require("express")
 const cors = require("cors")
+const sslRedirect = require('heroku-ssl-redirect');
 const courseRouter = require("./routes/course")
 
 const app = express()
 const port = process.env.PORT || 3000
 
+app.use(sslRedirect()); // enable ssl redirect
 app.use(cors()) // allow access from all origins
 app.use(express.json()) // parse request as json
 app.use(courseRouter)
