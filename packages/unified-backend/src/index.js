84ada772c9ac94de930142e422f4d5b952c1ba71
Shawn Hu <shanwhu97@gmail.com>
2020-06-30 12:48:04 -0400

    more https testing

--- packages/unified-backend/src/index.js
+++ packages/unified-backend/src/index.js
@@ -1,16 +1,24 @@
 require("./db/mongoose")
 const express = require("express")
 const cors = require("cors")
-const sslRedirect = require('heroku-ssl-redirect');
 const courseRouter = require("./routes/course")
 
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(sslRedirect()); // enable ssl redirect
 app.use(cors()) // allow access from all origins
 app.use(express.json()) // parse request as json
 app.use(courseRouter)
+
+if (process.env.NODE_ENV === 'production') {
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+}
+
 
 app.get("/", (req, res) => {
     res.redirect('https://docs.viaplanner.ca/course-api/');
