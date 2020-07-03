29e6aae433586d617e977e81b3c3ce719e2afbea
TheFatPanda97 <shawnhu97@gmail.com>
2020-07-03 16:42:05 -0400

    added background img

--- packages/app/src/components/SideBar.vue
+++ packages/app/src/components/SideBar.vue
@@ -3,23 +3,31 @@
         <v-card :height="coursePanelHeight" class="pa-4">
             <h1 class="text-h6">{{ sideBarTitle }}</h1>
             <hr class="mb-1" />
-            <smooth-scrollbar class="right-scroll-area">
-                <v-expansion-panels
-                    :v-model="whichCoursesExpanded"
-                    multiple
-                    hover
-                    class="expansion-panel-settings pa-1"
-                >
-                    <selected-course-card
-                        v-for="(course, code) in filterCourses"
-                        :key="code"
-                        :course="course"
-                    />
-                </v-expansion-panels>
-            </smooth-scrollbar>
+            <v-row
+                justify="center"
+                align="center"
+                :style="`height: ${coursePanelHeight * 0.8}px`"
+                style="z-index: -1"
+            >
+                <img :src="imgSrc" style="position: absolute" :width="imgWidth" />
+                <smooth-scrollbar class="right-scroll-area">
+                    <v-expansion-panels
+                        :v-model="whichCoursesExpanded"
+                        multiple
+                        hover
+                        class="expansion-panel-settings pa-1"
+                    >
+                        <selected-course-card
+                            v-for="(course, code) in filterCourses"
+                            :key="code"
+                            :course="course"
+                        />
+                    </v-expansion-panels>
+                </smooth-scrollbar>
+            </v-row>
         </v-card>
         <v-card :height="programPanelHeight" class="mt-3 pa-4">
-            <v-overlay absolute opacity="0.3" z-index=1>
+            <v-overlay absolute opacity="0.3" z-index="1">
                 <h1>Coming Soon</h1>
             </v-overlay>
             <h1 class="text-h6">Programs</h1>
@@ -58,6 +66,16 @@ export default {
         programPanelHeight() {
             return (window.innerHeight - 99) * 0.4;
         },
+        imgWidth() {
+            return window.innerWidth * 0.17;
+        },
+        imgSrc() {
+            if (this.getSemesterStatus === "F") {
+                return require("../assets/fall-background.png");
+            } else {
+                return require("../assets/winter-background.png");
+            }
+        },
         filterCourses() {
             this.timetable; //force re-render the selected courses
             const filteredCourses = {};
@@ -77,6 +95,7 @@ export default {
 .right-scroll-area {
     position: relative;
     height: 90% !important;
+    background-color: transparent;
 }
 .expansion-panels-settings {
     width: 90%;
