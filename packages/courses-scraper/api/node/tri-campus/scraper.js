7296794500076497438adf4b08c1273a34b1ac33
TheFatPanda97 <shawnhu97@gmail.com>
2020-07-12 00:56:47 -0400

    testing cluster library

--- packages/courses-scraper/api/node/tri-campus/scraper.js
+++ packages/courses-scraper/api/node/tri-campus/scraper.js
@@ -1,36 +1,64 @@
 const puppeteer = require('puppeteer');
-const fs = require("fs")
+const { Cluster } = require('puppeteer-cluster');
+// const fs = require("fs")
 
 
 const scrape = async () => {
-    const browser = await puppeteer.launch({
-        executablePath: "/usr/bin/chromium-browser"
+    // const browser = await puppeteer.launch({
+    //     executablePath: "/usr/bin/chromium-browser"
+    // });
+    // const page = await browser.newPage();
+    // await page.goto('https://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start#search', { waitUntil: 'networkidle0' });
+    // // await page.select("select[name='courseSearchResults_length']", "100")
+
+    // let coursesRawInfo = await page.evaluate(() => {
+    //     let allData = []
+    //     allData.push(document.querySelector("div.dataTables_info").innerText)
+
+    //     // return document.querySelector("tr.odd").querySelectorAll("td")[1].innerText
+    //     // document.querySelector("tr.odd").querySelectorAll("td")[1].querySelector("a").click()
+
+
+    //     // test.forEach((element, index, arr)=>{
+    //     //     arr[index] = element.innerText
+    //     // })
+
+    //     return allData
+
+    // })
+
+    // console.log(coursesRawInfo)
+
+    // await browser.close();
+
+    const cluster = await Cluster.launch({
+        concurrency: Cluster.CONCURRENCY_CONTEXT,
+        maxConcurrency: 2,
+        monitor: true,
+        puppeteerOptions: { executablePath: "/usr/bin/chromium-browser" }
     });
-    const page = await browser.newPage();
-    await page.goto('https://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start#search', { waitUntil: 'networkidle0' });
-    // await page.select("select[name='courseSearchResults_length']", "100")
 
-    let coursesRawInfo = await page.evaluate(() => {
-        let allData = []
-        allData.push(document.querySelector("div.dataTables_info").innerText)
+    await cluster.task(async ({ page, data: url }) => {
+        await page.goto(url, { waitUntil: 'networkidle0' });
+        await page.select("select[name='courseSearchResults_length']", "100")
 
-        // return document.querySelector("tr.odd").querySelectorAll("td")[1].innerText
-        // document.querySelector("tr.odd").querySelectorAll("td")[1].querySelector("a").click()
+        let coursesRawInfo = await page.evaluate(() => {
+            let allData = []
+            allData.push(document.querySelector("div.dataTables_info").innerText)
+            return allData
+        })
 
+        console.log(coursesRawInfo)
 
-        // test.forEach((element, index, arr)=>{
-        //     arr[index] = element.innerText
-        // })
+    });
 
-        return allData
+    cluster.queue('https://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start#search');
+    cluster.queue('https://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start#search');
+    // cluster.queue('http://www.wikipedia.org/');
+    // many more pages
 
-    })
-
-    console.log(coursesRawInfo)
-
-    await browser.close();
-
-
+    await cluster.idle();
+    await cluster.close();
 }
 
 scrape()
