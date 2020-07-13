b45d4a64eea6f8f9ad315d6be285cb655963c04e
TheFatPanda97 <shawnhu97@gmail.com>
2020-07-12 23:19:26 -0400

    testing on csc108

--- packages/courses-scraper/api/node/tri-campus/scraper.js
+++ packages/courses-scraper/api/node/tri-campus/scraper.js
@@ -1,18 +1,36 @@
 import { Cluster } from 'puppeteer-cluster';
-// const fs = require("fs")
+import Course from "../structures/course"
+import MeetingSection from "../structures/meetingSection"
+import Time from "../structures/time"
+import fs from "fs"
 
-const formatCode = (rawCourseCode, rawTerm) => {
+const formatID = (rawCourseCode, rawTerm) => {
     let strippedTerm = rawTerm.split(" ")
     let year = parseInt(strippedTerm[0])
     let semester = strippedTerm[1]
 
-    if(semester === "Fall"){
+    if (semester === "Fall") {
         return `${rawCourseCode}F${year}9`
     }
-    else if(semester === "Winter"){
+    else if (semester === "Winter") {
         return `${rawCourseCode}S${year}1`
     }
-    else{
+    else {
+        return ""
+    }
+}
+
+const formatCourseCode = (rawCourseCode, rawTerm) => {
+    let strippedTerm = rawTerm.split(" ")
+    let semester = strippedTerm[1]
+
+    if (semester === "Fall") {
+        return `${rawCourseCode}F`
+    }
+    else if (semester === "Winter") {
+        return `${rawCourseCode}S`
+    }
+    else {
         return ""
     }
 }
@@ -22,16 +40,57 @@ const scrape = async () => {
         concurrency: Cluster.CONCURRENCY_CONTEXT,
         maxConcurrency: 10,
         monitor: true,
-        puppeteerOptions: { executablePath: "/usr/bin/chromium-browser" }
+        puppeteerOptions: { executablePath: "/usr/bin/chromium-browser" },
     });
 
-    await cluster.task(async ({ page, data: url }) => {
-        await page.goto(url, { waitUntil: 'networkidle0' });
-        await page.select("select[name='courseSearchResults_length']", "100")
-
+    // Event handler to be called in case of problems
+    cluster.on('taskerror', (err, data) => {
+        console.log(`Error crawling ${data}: ${err.message}`);
     });
 
-    cluster.queue('https://coursefinder.utoronto.ca/course-search/search/courseSearch?viewId=CourseSearch-FormView&methodToCall=start#search');
+
+    await cluster.task(async ({ page, data }) => {
+        await page.goto(data.url, { waitUntil: 'networkidle0' });
+        let currCourse = new Course()
+        currCourse.setCourseCode(data.courseCode)
+        currCourse.setTerm(data.term)
+
+        let rawCourseData = await page.evaluate(() => {
+
+            let courseInfo = {
+                rawName: document.querySelector("span.uif-headerText-span").innerText,
+                rawDescription: document.querySelector("span[id='u32']").innerText,
+                rawDivision: document.querySelector("span[id='u23']").innerText,
+                rawDepartment: document.querySelector("span[id='u41']").innerText, 
+                rawPrerequisites: document.querySelector("span[id='u50']") != null ? document.querySelector("span[id='u50']").innerText : "", 
+                rawExclusions: document.querySelector("span[id='u68']") != null ? document.querySelector("span[id='u68']").innerText : "", 
+                rawCampus: document.querySelector("span[id='u149']").innerText,
+                rawBreadth: document.querySelector("span[id='u12']") != null ?document.querySelector("span[id='u12']").innerText : "",
+                rawDistribution: document.querySelector("span[id='u122']") != null ? document.querySelector("span[id='u122']").innerText : "",
+                rawMeetingSections: []
+            }
+
+            return courseInfo
+        })
+
+        // console.log(currCourse)
+        console.log(rawCourseData)
+    });
+
+    // let rawInfo = fs.readFileSync("output/allCourseCodes.json");
+    // let allCourseInfo = JSON.parse(rawInfo)
+    let allCourseInfo = [{ courseCode: "CSC108H5", term: "2020 Fall" }]
+    let baseURL = "https://coursefinder.utoronto.ca/course-search/search/courseInquiry?methodToCall=start&viewId=CourseDetails-InquiryView&courseId="
+
+    allCourseInfo.forEach((currCourseInfo) => {
+        let courseID = formatID(currCourseInfo.courseCode, currCourseInfo.term)
+        let fullCourseCode = formatCourseCode(currCourseInfo.courseCode, currCourseInfo.term)
+
+        if (courseID !== "") {
+            cluster.queue({ url: `${baseURL}${courseID}`, courseCode: fullCourseCode, term: currCourseInfo.term })
+        }
+
+    })
 
     await cluster.idle();
     await cluster.close();
