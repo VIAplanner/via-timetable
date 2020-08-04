274107a7b62ca34bacdb8022fe56fca9441b7984
TheFatPanda97 <shawnhu97@gmail.com>
2020-08-03 23:10:51 -0400

    further refactoring

--- packages/courses-scraper/api/python/example_usage.py
+++
@@ -1,3 +0,0 @@
-import uoftscraper
-
-uoftscraper.Courses.scrape()
--- packages/unified-backend/src/db/mongoose.js
+++
@@ -1,4 +0,0 @@
-const mongoose = require("mongoose")
-require('dotenv').config()
-
-mongoose.connect(process.env.MONGODB_URL, { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true })
