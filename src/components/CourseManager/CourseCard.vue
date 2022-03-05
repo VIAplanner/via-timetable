1f4242c1edfa8a0ca32aee685efb6153c36af129
Giang Bui <bhuonggiang03@gmail.com>
2022-03-05 18:46:44 -0500

    add styles to course manager page and fix bugs

--- src/components/CourseManager/CourseCard.vue
+++ src/components/CourseManager/CourseCard.vue
@@ -1,30 +1,33 @@
 <template>
-    <div class="course-card">
-        <button>{{ courseCode }}</button>
-        <p>{{ name }}</p>
-        <button>A</button>
-        <button>B</button>
-        <button>C</button>
-        <button>D</button>
-    </div>
+  <v-card class="mx-auto course-card">
+    <v-list-item>
+      <v-row align="center" justify="space-between" style="margin: auto">
+        <div class="text-h5 text--primary">
+          {{ courseCode }}
+        </div>
+        <div class="text-h5 text--primary">{{ name }}</div>
+        <div align="center" justify="end" style="width: max-content">
+          <v-icon class="mr-1"> mdi-download </v-icon>
+          <v-icon class="mr-1"> mdi-square-edit-outline </v-icon>
+          <v-icon class="mr-1"> mdi-delete </v-icon>
+          <v-icon class="mr-1"> mdi-chevron-right </v-icon>
+        </div>
+      </v-row>
+    </v-list-item>
+  </v-card>
 </template>
 
 <script>
 export default {
-    props: {
-        name: String,
-        courseCode: String
-    }
-}
+  props: {
+    name: String,
+    courseCode: String,
+  },
+};
 </script>
 
 <style scoped>
-
 .course-card {
-    min-height: 100%;
-    display: flex;
-    flex-direction: row;
-    flex-wrap: wrap;
+    margin-bottom: 16px;
 }
-
 </style>
--- src/views/Manager.vue
+++ src/views/Manager.vue
@@ -1,45 +1,60 @@
 <template>
-  <v-row>
+  <v-row class="main">
     <v-col>
       <v-sheet :height="managerHeight">
-        <!-- TODO MANAGER UI -->
         <h1>Course Manager</h1>
-        <v-btn
-          elevation="2"
-        >Add a course</v-btn>
-        <br>
-        <CourseCard
-          name="Intro to Computer Science"
-          courseCode="CSC148"
-        ></CourseCard>
+        <v-btn elevation="2" style="margin: 24px 0">Add a course</v-btn>
+        <div v-for="course in courses" :key="course.courseCode">
+          <course-card
+            :name="course.name"
+            :courseCode="course.courseCode"
+          ></course-card>
+        </div>
       </v-sheet>
     </v-col>
   </v-row>
 </template>
 
 <script>
-  export default {
-    created() {
-      window.addEventListener('resize', this.handleResize);
+import CourseCard from '../components/CourseManager/CourseCard.vue';
+
+export default {
+  components: {
+    CourseCard,
+  },
+  created() {
+    window.addEventListener('resize', this.handleResize);
+  },
+  data() {
+    return {
+      height: window.innerHeight,
+      courses: [
+        {
+          name: 'Intro to Computer Science',
+          courseCode: 'CSC148',
+        },
+        {
+          name: 'Intro to Computer Science',
+          courseCode: 'CSC148',
+        },
+      ],
+    };
+  },
+  methods: {
+    handleResize() {
+      this.height = window.innerHeight;
     },
-    data() {
-      return {
-        height: window.innerHeight,
-      }
+  },
+  computed: {
+    managerHeight() {
+      return this.height - 104;
     },
-    methods: {
-      handleResize() {
-        this.height = window.innerHeight;
-      },
-    },
-    computed: {
-      managerHeight() {
-        return this.height - 104;
-      },
-    },
-  }
+  },
+};
 </script>
 
 <style lang="scss" scoped>
-
+.main {
+  margin: 56px;
+}
 </style>
