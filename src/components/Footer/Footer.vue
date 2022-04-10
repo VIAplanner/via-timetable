05a25a398fc19b41e636bf220518880d445ed506
Giang Bui <bhuonggiang03@gmail.com>
2022-04-09 20:33:30 -0400

    auto update for copyright

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -4,8 +4,8 @@
       <v-footer class="white">
         <v-row>
           <v-col class="pa-0">
-            <h1 style="text-align:center" class="text-subtitle-1">
-              Copyright © 2021 VIAplanner - Data updated for the 2021 - 2022
+            <h1 style="text-align: center" class="text-subtitle-1">
+              Copyright © {{ new Date().getFullYear() }} VIAplanner - Data updated for the 2021 - 2022
               school year
             </h1>
           </v-col>
@@ -16,7 +16,5 @@
 </template>
 
 <script>
-  export default {
-    
-  }
+export default {};
 </script>
