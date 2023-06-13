bd7ddead50081effc8d1951d7e1e4d89df6eab32
Ido <benhaim.ido@gmail.com>
2023-06-13 18:23:07 -0400

    Added tooltip

--- src/components/AppBar/ChangeTheme.vue
+++ src/components/AppBar/ChangeTheme.vue
@@ -4,9 +4,14 @@
       @click='()=>setDarkMode($vuetify.theme.dark = !$vuetify.theme.dark)'
       icon
     >
-      <v-icon>
-        {{ !$vuetify.theme.dark ? 'mdi-lightbulb' : 'mdi-brightness-4' }}
-      </v-icon>
+      <v-tooltip bottom>
+        <template v-slot:activator='{ on, attrs }'>
+          <v-icon v-bind='attrs' v-on='on'>
+            {{ !$vuetify.theme.dark ? 'mdi-lightbulb' : 'mdi-brightness-4' }}
+          </v-icon>
+        </template>
+        <span>Change theme</span>
+      </v-tooltip>
     </v-btn>
   </div>
 </template>
